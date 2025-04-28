8f001ac86d2b5857b7e7f19fbad44be9d2989fb3
julio candeia <juliocandeia594@gmail.com>
2025-04-28 00:44:32 -0300

    feat: create route to login user

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/User/userRegisterController.ts';
+import { UserLoginController } from '../controllers/User/userLoginController.ts';
 
 const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.post('/register', (req, res) => userController.register(req, res));
+userRoutes.post('/login', (req, res) => {
+  const userLoginController = new UserLoginController();
+  userLoginController.login(req, res);
+});
+
 
 export { userRoutes };
